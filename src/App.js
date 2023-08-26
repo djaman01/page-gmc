8b73e6157ad12b71675b710a7f4b7678974acac5
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-08-26 18:39:44 +0100

    bonne avancée head et side

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './Header.js'
 import Sidebar from './Sidebar.js'
+import Body from './Body.js'
 
 
 function App() {
   return (
    <>
+    
    <Header/>
    <Sidebar/>
+   <Body/>
    </>
   );
 }
---
+++ src/Body.js
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function Body() {
+  return (
+    <div>
+      <button>web fundamentals</button>
+
+    </div>
+  )
+}

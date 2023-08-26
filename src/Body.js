311ef9cc63cfbefb2221bb71f6fb6695423c1700
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-08-27 00:01:04 +0100

    sauvegarde juste pour etre sur

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,10 @@
 import './App.css'
 
 export default function Body() {
+
   return (
     <div className='check-pic'>
       <img src="/CheckpointImg.svg" alt="check-image"/>
-      <button>Hey</button>
     </div>
   )
 }
